1d7c8cf4445617214427de919ac0e4b41e6c4405
chimingg <68934510+chimingg@users.noreply.github.com>
2020-10-31 02:26:58 +0700

    Add files via upload

---
+++ 7.3.php
@@ -0,0 +1,16 @@
+<?php
+/*ID: 61211070
+Name: zhiming zheng
+ */
+function calculate($c)
+{  
+	$f=($c*9/5)+32;	
+	return $f;
+}
+$file = file_get_contents($argv[1]);
+$line = explode("\r\n",$file);
+$n = $line[0]; 
+for($i=1;$i<=$n;$i++)
+{ 
+	echo "\t".$line[$i]."\t".calculate($line[$i])."\n";
+}
